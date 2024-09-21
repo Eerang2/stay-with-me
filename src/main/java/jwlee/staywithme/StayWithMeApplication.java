494c6ef2d78aa0d980d25cb6ef21cdff315955d8
jinwoo <dlwlsdn8022@gmail.com>
2024-09-21 15:56:02 +0900

    Initial commit

---
+++ src/main/java/jwlee/staywithme/StayWithMeApplication.java
@@ -0,0 +1,13 @@
+package jwlee.staywithme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayWithMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StayWithMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jwlee/staywithme/StayWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package jwlee.staywithme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayWithMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
